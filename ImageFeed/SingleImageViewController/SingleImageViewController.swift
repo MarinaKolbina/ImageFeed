b458d597d8bea0df81ea4e5aca91b298ef2bac3b
MarinaKolbina <marianna.hana@gmail.com>
2022-12-22 20:30:21 +0100

    Реализован переход с таблицы к SingleImageView

---
+++ ImageFeed/SingleImageViewController/SingleImageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Marina Kolbina on 22/12/2022.
+//
+
+import Foundation
+import UIKit
+
+class SingleImageViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
