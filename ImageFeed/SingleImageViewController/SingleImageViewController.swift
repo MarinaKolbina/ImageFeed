4d171248fe472f7668f1590350b3c9f87107f472
MarinaKolbina <marianna.hana@gmail.com>
2022-12-22 22:47:31 +0100

    Реализован сегвей

--- ImageFeed/SingleImageViewController/SingleImageViewController.swift
+++ ImageFeed/SingleImageViewController/SingleImageViewController.swift
@@ -9,6 +9,19 @@ import Foundation
 import UIKit
 
 class SingleImageViewController: UIViewController {
+    
     @IBOutlet weak var imageView: UIImageView!
+
+    var image: UIImage! {
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+        }
+    }
+
+    override func viewDidLoad() {
+            super.viewDidLoad()
+            imageView.image = image
+        }
     
 }
