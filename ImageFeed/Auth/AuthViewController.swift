17a214958acb9f163857fc9b15dd2d2148dd84bf
MarinaKolbina <marianna.hana@gmail.com>
2023-01-07 13:25:53 +0100

    Подготовлено все до POST-запроса

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Marina Kolbina on 03/01/2023.
+//
+
+import Foundation
+
+class AuthViewController {
+    private let ShowWebViewSegueIdentifier = "ShowWebView"
+    
+}
---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Marina Kolbina on 02/01/2023.
+//
+
+import Foundation
+
+let AccessKey = "Gbw9y_w3yiafGdKoQ9NUMG6vKdjnsKRx1nbwoGIe6OM"
+let SecretKey = "gsYaWk6sp-JW2yNKO_hKHGdRSpOvfZiNncHkYw7xirI"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
