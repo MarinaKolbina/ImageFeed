f70cea68ed28f17eb06f2ed95e4f639c8a9b04c6
MarinaKolbina <marianna.hana@gmail.com>
2023-01-21 23:10:39 +0100

    Исправлены замечания после ревью

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -17,7 +17,7 @@ final class AuthViewController: UIViewController {
     private let oAuth2Service = OAuth2Service()
     private var oAuth2TokenStorage: OAuth2TokenStorageProtocol = OAuth2TokenStorage()
     weak var delegate: AuthViewControllerDelegate?
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == ShowWebViewSegueIdentifier {
             guard
@@ -33,19 +33,21 @@ final class AuthViewController: UIViewController {
 
 extension AuthViewController: WebViewViewControllerDelegate {
     func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
-        oAuth2Service.fetchOAuthToken(code) { [weak self] result in
-            guard let self = self else { return }
-            switch result {
+        DispatchQueue.main.async {
+            self.oAuth2Service.fetchOAuthToken(code) { [weak self] result in
+                guard let self = self else { return }
+                switch result {
                 case .success(let token):
                     self.oAuth2TokenStorage.token = token
                     self.delegate?.authViewController(self, didAuthenticateWithCode: token)
-        
+                    
                 case .failure(let error):
-                        print(error)
+                    print(error)
+                }
             }
         }
     }
-
+    
     func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
         dismiss(animated: true)
     }
